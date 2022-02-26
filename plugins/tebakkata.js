712f9bff8e8bf317337375692c4b404c38636eb6
Alan Adi P <96643671+WallHackBotz@users.noreply.github.com>
2022-02-26 15:09:07 +0700

    Update tebakkata.js

--- plugins/tebakkata.js
+++ plugins/tebakkata.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let timeout = 120000
-let poin = 5000
+let poin = 3000
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
