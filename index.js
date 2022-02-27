d965deb11c8f3601b2c708986b25e35c75a1ee84
Alan Adi P <96643671+WallHackBotz@users.noreply.github.com>
2022-02-28 05:27:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Rpg\nKingOfBear', {
+CFonts.say('Rpg\nALAN BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
