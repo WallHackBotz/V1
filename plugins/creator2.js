28046437729b1c61a2ce0360c5a51dd6ff788d8a
Alan Adi P <96643671+WallHackBotz@users.noreply.github.com>
2022-04-06 17:19:51 +0700

    Add files via upload

--- plugins/creator2.js
+++ plugins/creator2.js
@@ -3,17 +3,17 @@ const PhoneNumber = require('awesome-phonenumber')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   await conn.sendMessage(m.chat, {
     contacts: [{
-    "displayName": "ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ᴏᴡɴᴇʀ ᴋᴀɴɴᴀʙᴏᴛ\nitem1.TEL;waid=62895328255152:62895328255152\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:alanadipermana261@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://lolhuman.xyz/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ 𝗥𝗥𝗦𝗦𝗭𝗫𝗫 𝗕𝗢𝗧 ]───────\nEND:VCARD"
+    "displayName": "𝙾𝚠𝚗𝚎𝚛 𝚁𝚛𝚜𝚜𝚣𝚡𝚡",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:𝙾𝚠𝚗𝚎𝚛 𝚁𝚛𝚜𝚜𝚣𝚡𝚡\nitem1.TEL;waid=62895328255152:62895328255152\nitem1.X-ABLabel:📍 Creator\nitem2.EMAIL;type=INTERNET:rrsszxx98@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://api-invibot.herokuapp.com/\nitem3.X-ABLabel:📮 Rest Api\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:🌍 Region | Otaku 🇯🇵\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
   }, {
-    "displayName": "Whatsapp",
-      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:Whatsapp\nitem1.TEL;waid=0:0\nitem1.X-ABLabel:📍 Whatsapp\nitem2.EMAIL;type=INTERNET:Whatsapp@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://faq.whatsapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Whatsapp;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
+    "displayName": "ʀʀꜱꜱᴢxx ʙᴏᴛ",
+      "vcard": "BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:ʀʀꜱꜱᴢxx ʙᴏᴛ\nitem1.TEL;waid=62895401928948:62895401928948\nitem1.X-ABLabel:📍 Bot Whatsapp\nitem2.EMAIL;type=INTERNET:rrsszxx98@gmail.com\nitem2.X-ABLabel:💌 Email\nitem3.URL:https://api-invibot.herokuapp.com/\nitem3.X-ABLabel:⚙️ Rest Api\nitem4.ADR:;;⛩️ Japan;;;;\nitem4.X-ABADR:ac\nitem4.X-ABLabel:📞 Official Bot Whatsapp\nitem5.X-ABLabel:───────[ ᴠᴇʀɪғʏ ʙʏ ᴡʜᴀᴛsᴀᴘᴘ ]───────\nEND:VCARD"
     }]
   }, MessageType.contactsArray, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
 
-handler.command = /^(nowner)$/i
+handler.command = /^(owner)$/i
 
 module.exports = handler
